22ca78da76f0eaec595928e0f3aec7097b79abb6
Fran Aliss <franalissar@gmail.com>
2022-04-24 17:23:23 -0400

    Ciclo TDD completado para los primeros 2 tests

--- src/torosyvacas.spec.js
+++ src/torosyvacas.spec.js
@@ -3,10 +3,20 @@ describe("Toros y vacas ->  ganar", () => {
         const torosyvacas = new TorosVacas("3");
         expect(torosyvacas.adivinar("3")).toEqual("ganaste");
     });
+    it("Si no se adivina la cadena devolver '' (vacio)", ()=>{
+        const torosyvacas = new TorosVacas("3");
+        expect(torosyvacas.adivinar("4")).toEqual("");
+    });
 });
 
 class TorosVacas{
+    constructor(codSecreto){
+        this.codSecreto = codSecreto;
+    }
     adivinar(intento){
+        if(intento === this.codSecreto){
         return "ganaste";
+        }
+        return "";
     }
 }
