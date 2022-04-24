aa5ee0183dbb7f3b59f15d2488790e58d90a9c57
Fran Aliss <franalissar@gmail.com>
2022-04-24 17:18:40 -0400

    Primer error, primer test

---
+++ src/torosyvacas.spec.js
@@ -0,0 +1,7 @@
+describe("Toros y vacas ->  ganar", () => {
+    it("Si se adivino la cadena devolver ganaste", ()=>{
+        const torosyvacas = new TorosVacas("3");
+        expect(torosyvacas.adivinar("3")).toEqual("ganaste");
+    });
+});
+
